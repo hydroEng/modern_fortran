a9690a3dd1ca5c0d627f86a81baf8242dd5e07d2
hydroEng <tahasoomro888@gmail.com>
2023-03-12 09:14:17 +1100

    Initial commit

---
+++ tsunami.f90
@@ -0,0 +1,15 @@
+program tsunami
+    implicit none ! force explicit typing
+    
+    integer :: i, n
+    integer :: grid_size
+    integer :: num_time_steps
+
+    ! Initialize declared variables.
+
+    real, parameter :: dt = 1, dx = 1, c = 1
+
+    grid_size = 100
+    num_time_steps = 100
+
+end program tsunami
