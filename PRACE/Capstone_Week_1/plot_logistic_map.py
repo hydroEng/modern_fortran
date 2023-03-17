4b8c90fb23a0686ed3168848ac82f8198daa954c
Taha S <tahasoomro888@gmail.com>
2023-03-17 21:14:39 +1100

    new plots

---
+++ PRACE/Capstone_Week_1/plot_logistic_map.py
@@ -0,0 +1,13 @@
+import seaborn as sns
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+data = pd.read_csv('q1_out.csv')
+data = data.dropna()
+data.index = data['r']
+data = data.drop(labels = 'r', axis=1)
+
+sns.scatterplot(data = data, s = 4)
+
+plt.savefig('plot.png', dpi = 400)
